9d70f891eb95d0a3004ac594466993e780739476
Vadim Dudkevich <justin_duda@ukr.net>
2021-12-13 18:54:57 +0200

    Added files
    Created a gallery with the ability to click on its elements and view the full-size image in a modal window.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemMarkup());
+
+console.log(galleryContainer);
+
+galleryContainer.addEventListener('click', onShowImg);
+
+function galleryItemMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class = "gallery__item">
+    <a class = "gallery__link" href = "${original}">
+    <img
+    class = "gallery__image"
+    src = "${preview}"
+    data-source = "${original}"
+    alt = "${description}"
+    />
+    </a>
+    </div>`;
+    })
+    .join('');
+}
+
+function onShowImg(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  let bigImageSrc = e.target.dataset.source;
+
+  const modal = basicLightbox.create(
+    `<img src="${bigImageSrc}" width="800" height="600">`,
+  );
+  modal.show();
+
+  if (modal.visible()) {
+    window.addEventListener('keydown', onPressKeyESC);
+  }
+
+  function onPressKeyESC(e) {
+    if (e.code === 'Escape') {
+      modal.close();
+      window.removeEventListener('keydown', onPressKeyESC);
+    }
+  }
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemMarkup());
+
+console.log(galleryContainer);
+
+function galleryItemMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class = "gallery__item">
+    <a class = "gallery__link" href = "${original}">
+    <img
+    class = "gallery__image"
+    src = "${preview}"
+    data-source = "${original}"
+    alt = "${description}"
+    />
+    </a>
+    </div>`;
+    })
+    .join('');
+}
+
+// const galleryContainer = document.querySelector('.gallery');
+
+// console.log(galleryContainer);
+
+// galleryContainer.addEventListener('click', onShowImg);
+
+// (function galleryItemMarkup() {
+//   const galleryMarkup = galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `<div class = "gallery__item">
+//     <a class = "gallery__link" href = "${original}">
+//     <img
+//     class = "gallery__image"
+//     src = "${preview}"
+//     data-source = "${original}"
+//     alt = "${description}"
+//     />
+//     </a>
+//     </div>`;
+//     })
+//     .join('');
+//   galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+// })();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
+
 console.log(galleryItems);
